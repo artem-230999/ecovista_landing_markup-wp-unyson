171fa28fd2a78fa5a4e5e3fbee43e5d465f2c517
Artem Abramov <artemabramov230999@gmail.com>
2024-11-02 18:48:25 +0100

    2-add destion banner and features

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
 	
+  const swiperBanner = new Swiper('.swiper-banner', {
+    speed: 700,
+    spaceBetween: 50,
+    slidesPerView: 1,
+    pagination: {
+      enabled: true,
+      el: '.swiper-banner .swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      enabled: true,
+      nextEl: '.swiper-banner .swiper-button-next',
+      prevEl: '.swiper-banner .swiper-button-prev',
+      clickable: true
+    }
+  });
 	
 
 }, false);
